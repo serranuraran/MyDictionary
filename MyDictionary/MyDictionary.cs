cb1e0ec28fe691d85f05e35654e7b5d6138bf626
90554 <90554@DESKTOP-K3RAL3M>
2021-01-24 20:26:58 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] keys;
+        T2[] values;
+
+        public MyDictionary()
+        {
+            keys = new T1[0];
+            values = new T2[0];
+        }
+
+        public void Add(T1 key, T2 value)
+        {
+            T1[] tempKeys = keys;
+            T2[] tempValues = values;
+            keys = new T1[keys.Length + 1];
+            values = new T2[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public (T1, T2)[] Items
+        {
+            get
+            {
+                List<(T1, T2)> items = new List<(T1, T2)>();
+                for (int i = 0; i < values.Length; i++)
+                {
+                    items.Add((keys[i], values[i]));
+                }
+                return items.ToArray();
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> sehirler = new MyDictionary<int, string>();
+            sehirler.Add(48, "Muğla");
+            sehirler.Add(35, "İzmir");
+            sehirler.Add(15, "Burdur");
+            sehirler.Add(32, "Isparta");
+            sehirler.Add(34, "İstanbul");
+            sehirler.Add(09, "Aydın");
+            sehirler.Add(20, "Denizli");
+
+            foreach (var sehir in sehirler.Items)
+            {
+                Console.WriteLine("Plaka: " + sehir.Item1 + "\tŞehir: " + sehir.Item2);
+            }
+
+            Console.WriteLine("Plakası ve İsmi Yazılan İl Sayısı " + sehirler.Items.Length);
+
+        }
+    }
+}
